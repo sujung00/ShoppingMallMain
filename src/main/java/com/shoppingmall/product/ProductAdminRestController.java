f88eb911bb6bda2bc4d5e26460bf1a97494b434b
sujung00 <rlatnwjdirin@naver.com>
2023-05-13 23:28:23 +0900

    상품 create

--- src/main/java/com/shoppingmall/product/ProductAdminRestController.java
+++ src/main/java/com/shoppingmall/product/ProductAdminRestController.java
@@ -74,7 +74,7 @@ public class ProductAdminRestController {
 			@RequestParam(value="mainImage", required=false) MultipartFile mainImage,
 			@RequestParam("detailedInfo") String detailedInfo,
 			@RequestParam("gender") String gender,
-			@RequestParam("productImages") List<MultipartFile> files){
+			@RequestParam(value="productImages", required=false) List<MultipartFile> files){
 		// db update
 		int rowCount = productAdminBO.updateProductByproductId(productId, name, information, price, mainImage, detailedInfo, gender, files);
 		
